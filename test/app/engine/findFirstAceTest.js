b96061a18223155547f2b182c81c2df6cd1aa780
Amir jiryan <amirjiryan@gmail.com>
2020-04-03 21:04:25 +0330

    changed test files

--- test/app/engine/findFirstAceTest.js
+++ test/app/engine/findFirstAceTest.js
@@ -1,7 +1,7 @@
-var Cards = require('./../../../app/models/cards');
+var Cards = require('./../../../app/models/cards').Deck;
 const expect = require('chai').expect;
 var sinon = require('sinon');
-var findFirstace = require('./../../../app/engine/findFirstace');
+var findFirstace = require('./../../../app/engine/findFirstace').findFirstَََAce;
 describe('FindfirstAce', function () {
     it('should take deck of cards an return player index who has the first ace', function () {
         var object = new Cards();
--- test/app/engine/sortCardsTest.js
+++ test/app/engine/sortCardsTest.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const sortCards = require('./../../../app/engine/sortCards');
+const sortCards = require('./../../../app/engine/sortCards').sortCards;
 describe('SortCards', function () {
     it('should take an array of cards as argument and return the highest card', function () {
         var array = [[10, "khaj"], [6, "khaj"], [12, "khaj"]];
